f098c21b9454cfce4178883a3d6514e3fae631da
KarimYasser15 <karimyasser1510@gmail.com>
2024-12-20 01:05:30 +0200

    pod

--- app.js
+++ app.js
@@ -9,17 +9,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use("/api/auth", authRouter);
 app.use("/order", orderRouter);
 app.use("/admin", adminRouter);
 app.use("/courier",courierRouter);
-app.use("/*", (req, res, next) => {
-  res.status(404).json({
-    status: "Error",
-    message: "Path Not Found",
-  });
-});
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -27,6 +21,14 @@ app.get("/", (req, res) => {
     message: "Api is working",
   });
 });
+
+app.use("/*", (req, res, next) => {
+  res.status(404).json({
+    status: "Error",
+    message: "Path Not Found",
+  });
+});
+
 
 //const PORT = process.env.APP_PORT || 4000;
 const PORT = 4000;
